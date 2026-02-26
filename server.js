9a2b795dbc188d8cc0e9c8a30f8a16b21d95c699
jeoopz06-netizen <jeoopz.06@gmail.com>
2026-02-26 10:26:20 -0600

    Update server.js

--- server.js
+++ server.js
@@ -31,11 +31,16 @@ const isHttpsBase = BASE_URL.startsWith('https://');
 ========================= */
 
 const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // false para TLS
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  tls: {
+    rejectUnauthorized: false
+  }
 });
 
 /* =========================
